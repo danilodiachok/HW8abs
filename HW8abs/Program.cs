9e1af6e627006c604d222fb5dcf9c819d8d77f03
danilo_cj <danilo_cj@DESKTOP-BT8DAVV>
2023-10-23 15:31:29 +0300

    Add project files.

---
+++ HW8abs/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HW8abs
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Circle("Circle 1", 5.0));
+            shapes.Add(new Square("Square 1", 4.0));
+            shapes.Add(new Circle("Circle 2", 3.0));
+            shapes.Add(new Square("Square 2", 2.0));
+
+            
+            Console.WriteLine("Shapes Infa:");
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"Name: {shape.Name}, Area: {shape.Area()}, Perimeter: {shape.Perimeter()}");
+            }
+
+            
+            Shape maxPerimeterShape = shapes[0];
+            foreach (Shape shape in shapes)
+            {
+                if (shape.Perimeter() > maxPerimeterShape.Perimeter())
+                {
+                    maxPerimeterShape = shape;
+                }
+            }
+            Console.WriteLine($"\nThe largest perimeter is: {maxPerimeterShape.Name}");
+
+            
+            shapes.Sort();
+            Console.WriteLine("\nShapes Sorted by Area:");
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"Name: {shape.Name}, Area: {shape.Area()}, Perimeter: {shape.Perimeter()}");
+            }
+        }
+    }
+}
---
+++ HW8abs/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8abs
+{
+    public abstract class Shape : IComparable<Shape>
+    {
+        public string Name { get; protected set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public int CompareTo(Shape other)
+        {
+            return Area().CompareTo(other.Area());
+        }
+    }
+}
